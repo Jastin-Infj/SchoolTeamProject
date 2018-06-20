afd5f757759229df7c7e1aa090e5e2e0e8bd85c4
Jastin <takumiyokoo@gmail.com>
2018-06-20 22:39:21 +0900

    合体させておきました

---
+++ MazeCube/Assets/Script/PlayerCont.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCont : MonoBehaviour {
+    //-------------------------------------------------------
+    //仮のプレイヤ処理
+    //-------------------------------------------------------
+
+    public float movementSpeed; //ユニティ側で進む速度を指定
+    public float jumpPower; //ユニティ側で上方向に進む速度を指定
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) //wキーで前に進む
+        {
+            transform.position += transform.forward * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S)) //sキーで後ろに進む
+        {
+            transform.position -= transform.forward * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D)) //dキーで右に進む
+        {
+            transform.position += transform.right * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A)) //aキーで左に進む
+        {
+            transform.position -= transform.right * movementSpeed;
+        }
+        //if (Input.GetKey(KeyCode.Space)) //スペースを押している間上方向へに進む
+        //{
+        //    transform.position += transform.up * jumpPower;
+        //}
+        if (Input.GetKey(KeyCode.Z)) //Zを押すとプレイヤが1度ずつ回転
+        {
+            transform.Rotate(new Vector3(0, 0, 1));
+        }
+    }
+}
