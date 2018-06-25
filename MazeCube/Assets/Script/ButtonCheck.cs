8b3552f78aa611198a01d9b633349f781a105627
Jastin <takumiyokoo@gmail.com>
2018-06-25 21:49:34 +0900

    PlayerCameraとTitleSceceを追加

---
+++ MazeCube/Assets/Script/ButtonCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    //ボタンが押されたあとの処理
+    public void OnClick()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
