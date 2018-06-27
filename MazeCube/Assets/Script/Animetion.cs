9e29c6c54347f183012e938f4bbb7649681e243e
Jastin <takumiyokoo@gmail.com>
2018-06-27 09:20:11 +0900

    統合終了

---
+++ MazeCube/Assets/Script/Animetion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animetion : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.S))
+        {
+            this.animator.SetBool("Walk", true);
+        }
+	}
+}
