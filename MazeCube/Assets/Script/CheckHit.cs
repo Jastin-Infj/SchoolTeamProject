d8af31609a493cb42ed8384af544561b974dab57
Jastin <takumiyokoo@gmail.com>
2018-07-05 23:16:01 +0900

    CharaとBollの当たり判定を実装しました

---
+++ MazeCube/Assets/Script/CheckHit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckHit : MonoBehaviour {
+
+    public string[] Tag;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    //オブジェクトが衝突したとき
+    void OnCollisionEnter(Collision collision)
+    {
+        for(int i = 0; i < Tag.Length;++i)
+        {
+            if (collision.gameObject.tag == Tag[i])
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+    //オブジェクトが離れた時
+    void OnCollisionExit(Collision collision)
+    {
+       
+    }
+
+    //オブジェクトが触れている間
+    void OnCollisionStay(Collision collision)
+    {
+        
+    }
+}
