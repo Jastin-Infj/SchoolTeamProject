3ded35c0069454fd73c45c14ad7596b773eb96e9
Jastin <takumiyokoo@gmail.com>
2018-07-12 21:36:14 +0900

    カメラの処理をアタッチしました

--- MazeCube/Assets/Script/Camera/PlayerCameraPos.cs
+++ MazeCube/Assets/Script/Camera/PlayerCameraPos.cs
@@ -6,20 +6,28 @@ public class PlayerCameraPos : MonoBehaviour
 {
     //PlayerCameraをPlayerに追従させるスクリプト
     //-------------------------------------------------------------------------------------
-    //PlayerCameraにアタッチしてください。
+    //PlayerPosにアタッチしてください。
     //-------------------------------------------------------------------------------------
     //変数定義
-    public GameObject Player;       //プレイヤ
-    public GameObject PlayerCamera; //プレイヤカメラ
+    public GameObject player;       //プレイヤ
+    public GameObject playercamera; //プレイヤカメラ
+    public GameObject PlayerPos;    //プレイヤ座標
 
     // Use this for initialization
     void Start()
     {
-        PlayerCamera.transform.position = Player.transform.position + (new Vector3(0, 1, -4)); //プレイヤーカメラをプレイヤの座標の後ろ、上に設置。
-        PlayerCamera.transform.Rotate(new Vector3(20, 0, 0)); //ｘ軸に20度傾ける。
+        PlayerPos.transform.position = player.transform.position;
+        PlayerPos.transform.rotation = player.transform.rotation;
+        playercamera.gameObject.transform.Translate(0, 1, -3); //プレイヤーカメラをプレイヤの座標の後ろ、上に設置。
+        playercamera.transform.Rotate(new Vector3(20, 0, 0)); //ｘ軸に20度傾ける。
     }
     void Update()
     {
-        PlayerCamera.transform.position = Player.transform.position + (new Vector3(0, 1, -3f));
+
+        PlayerPos.transform.position = player.transform.position;
+        if (Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.Z)) //BボタンまたはキーボードZキーが押された場合
+        {
+            PlayerPos.transform.rotation = player.transform.rotation;  //プレイヤ向きを取得
+        }
     }
 }
